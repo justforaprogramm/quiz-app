f1122e64b12cc48a5fa23fcaefbb11a92011b98f
Justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2026-06-22 10:57:10 +0200

    edits for dev

--- .vscode/init_terminal.sh
+++ .vscode/init_terminal.sh
@@ -5,8 +5,8 @@ if [ -f ~/.bashrc ]; then
 fi
 
 # DevPod Check
-echo "Prüfe python.devpod..."
-if ! ssh -q -o ConnectTimeout=2 python.devpod exit; then
+echo "Prüfe devpod..."
+if ! ssh -q -o ConnectTimeout=2 quizapp.devpod exit; then
     echo "Starte DevPod..."
     devpod up .
 fi
@@ -15,7 +15,7 @@ echo "Verbinde mit python.devpod und aktiviere venv..."
 
 # Verbindet per SSH, springt in den Container und startet dort eine interaktive 
 # Bash-Shell, die direkt das venv im Container sourct.
-ssh -t python.devpod "bash --init-file <(echo '
+ssh -t quizapp.devpod "bash --init-file <(echo '
     if [ -f ~/.bashrc ]; then source ~/.bashrc; fi
     if [ -d .venv ]; then 
         source .venv/bin/activate
